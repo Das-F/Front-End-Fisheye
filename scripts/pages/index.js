8e151c6c98a794f86e39f1d6a5683e6ab36db5c6
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-04-23 20:35:34 +0200

    fetch function in javascript and index.html began

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,30 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+  try {
+    const photographerTemplate = await fetch("/data/photographers.json");
+    const photographerTemplateData = await photographerTemplate.json();
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    return { photographers: photographerTemplateData.photographers };
+  } catch (error) {
+    console.error("Erreur de chargement des photographes :", error);
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+    return { photographers: [] }; // renvoie une liste vide en cas d’erreur
+  }
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,25 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", name); // accessibilité
+    img.setAttribute("aria-label", name); // accessibilité
+    imgContainer.appendChild(img);
+    article.appendChild(imgContainer);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    article.appendChild(h2);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
