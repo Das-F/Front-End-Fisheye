50ef67875b6fcdcda9782400e47b8cdce9e3521b
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-04-24 16:02:47 +0200

    pictures compressed, paragraph about photographers added with const JS and CSS style

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-  const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
@@ -7,17 +7,29 @@ function photographerTemplate(data) {
     const article = document.createElement("article");
 
     const imgContainer = document.createElement("div");
-    imgContainer.classList.add("img-container");
+    imgContainer.classList.add("img-container"); // A REVOIR APRES TOUT INSERE SI PERTINENT OU PAS
     const img = document.createElement("img");
     img.setAttribute("src", picture);
     img.setAttribute("alt", name); // accessibilité
     img.setAttribute("aria-label", name); // accessibilité
     imgContainer.appendChild(img);
-    article.appendChild(imgContainer);
+    article.appendChild(imgContainer); // A REVOIR APRES TOUT INSERE SI PERTINENT OU PAS
 
     const h2 = document.createElement("h2");
     h2.textContent = name;
     article.appendChild(h2);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = `${city}, ${country}`;
+    article.appendChild(h3);
+
+    const h4 = document.createElement("h4");
+    h4.textContent = tagline;
+    article.appendChild(h4);
+
+    const p = document.createElement("p");
+    p.textContent = price + "€/jour";
+    article.appendChild(p);
 
     return article;
   }
