739e289464d594fa936275cda653100fc07428fc
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-04-25 17:12:12 +0200

    index.html finished with accessibility, focus and links

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -28,3 +28,54 @@ async function init() {
 }
 
 init();
+
+function photographerTemplate(data) {
+  const { name, portrait, city, country, tagline, price, id } = data;
+
+  const picture = `assets/photographers/${portrait}`;
+
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+
+    // Creation link - image + name
+    const link = document.createElement("a");
+    link.setAttribute("href", `photographer.html?id=${id}`);
+    link.setAttribute("aria-label", `${name}`);
+    link.classList.add("link-photographer");
+
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", name);
+    img.setAttribute("aria-label", name);
+    imgContainer.appendChild(img);
+    link.appendChild(imgContainer);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    link.appendChild(h2);
+    // Add link in article
+    article.appendChild(link);
+
+    // Block complementary informations
+    const infos = document.createElement("div");
+    infos.classList.add("photographer-infos");
+
+    const h3 = document.createElement("h3");
+    h3.textContent = `${city}, ${country}`;
+    article.appendChild(h3);
+
+    const h4 = document.createElement("h4");
+    h4.textContent = tagline;
+    article.appendChild(h4);
+
+    const p = document.createElement("p");
+    p.textContent = price + "€/jour";
+    article.appendChild(p);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
+}
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,36 +1,16 @@
 function photographerTemplate(data) {
-  const { name, portrait, city, country, tagline, price } = data;
+  const { name, portrait } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
-
-    const imgContainer = document.createElement("div");
-    imgContainer.classList.add("img-container"); // A REVOIR APRES TOUT INSERE SI PERTINENT OU PAS
     const img = document.createElement("img");
     img.setAttribute("src", picture);
-    img.setAttribute("alt", name); // accessibilité
-    img.setAttribute("aria-label", name); // accessibilité
-    imgContainer.appendChild(img);
-    article.appendChild(imgContainer); // A REVOIR APRES TOUT INSERE SI PERTINENT OU PAS
-
     const h2 = document.createElement("h2");
     h2.textContent = name;
+    article.appendChild(img);
     article.appendChild(h2);
-
-    const h3 = document.createElement("h3");
-    h3.textContent = `${city}, ${country}`;
-    article.appendChild(h3);
-
-    const h4 = document.createElement("h4");
-    h4.textContent = tagline;
-    article.appendChild(h4);
-
-    const p = document.createElement("p");
-    p.textContent = price + "€/jour";
-    article.appendChild(p);
-
     return article;
   }
   return { name, picture, getUserCardDOM };
